3f4808370fe0ca41226cde3e6801e20fcd32ce81
vi1801 <vidalwadi11@gmail.com>
2021-10-23 02:13:56 -0700

    Finished the program with given inputs.

--- Oct2021/src/main/java/TwoSum.java
+++ Oct2021/src/main/java/TwoSum.java
@@ -1,5 +1,14 @@
 public class TwoSum {
     public static void main(String[] args) {
-
+        int nums[] = {2,7,11,15};
+        int target = 9;
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] == target - nums[i]) {
+                        System.out.println(i);
+                        System.out.println(j);
+                }
+            }
+        }
     }
 }

2f7d7d823d14b26589448723e6b4a1e210169cde
vi1801 <vidalwadi11@gmail.com>
2021-10-24 14:30:55 -0700

    Finding the longest common prefix.

---
+++ Oct2021/src/main/java/LongestCommonPrefix.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class LongestCommonPrefix{
+    public String LongestCommonPrefix(String[] a) {
+        int size = a.length;
+        if(size == 0)
+        {
+            return "";
+        }
+        if(size == 1)
+        {
+            return a[0];
+        }
+        Arrays.sort(a);
+        int end = Math.min(a[0].length(), a[size-1].length());
+        int i = 0;
+        while(i < end && a[0].charAt(i) == a[size-1].charAt(i))
+        {
+            i++;
+        }
+        String pre = a[0].substring(0,i);
+        return pre;
+
+    }
+
+    public static void main(String[] args) {
+        LongestCommonPrefix lcp = new LongestCommonPrefix();
+        String[] input = {"apple","app","application"};
+        System.out.println("The longest common prefix is: "+ lcp.LongestCommonPrefix(input));
+    }
+}

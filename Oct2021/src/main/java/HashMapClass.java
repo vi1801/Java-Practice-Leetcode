e3082ed19882322e8c44b0f76d3580a8c3f10a8f
vi1801 <vidalwadi11@gmail.com>
2021-10-29 15:19:51 -0700

    using get method to access the value.

--- Oct2021/src/main/java/HashMapClass.java
+++ Oct2021/src/main/java/HashMapClass.java
@@ -4,8 +4,9 @@ public class HashMapClass {
     public static void main(String[] args) {
         HashMap<String, String> capitalCities = new HashMap<String, String>();
         capitalCities.put("England","London");
-        capitalCities.put("USA","California");
-        capitalCities.put("India","Gujarat");
+        capitalCities.put("USA","Washington DC");
+        capitalCities.put("India","Delhi");
         System.out.println(capitalCities);
+        System.out.println(capitalCities.get("England"));
     }
 }

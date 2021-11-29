1e9a5996497d845e48d1eddea0a9d8754a3fd49d
vi1801 <vidalwadi11@gmail.com>
2021-11-28 17:54:57 -0800

    Finding length of the last word.

--- Oct2021/src/main/java/RemoveElement.java
+++ Oct2021/src/main/java/RemoveElement.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 
 public class RemoveElement {
-    public int removeElement( int val) {
-        int i = 0;
-        Double[] nums = new Double[4];
+    public int removeElement(int val) {
+        int i;
+        //int[] nums = new int[4];
         Scanner scan = new Scanner(System.in);
-        //for (i = 0; i < 4; i++) {
-              //Double[] nums = new Double[4];
-        System.out.println(scan.nextDouble());
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != val) {
-                nums[i] = nums[j];
-                i++;
+
+        System.out.println("Enter values: ");
+        for (i = 0; i < 4; i++) {
+            int[] nums = new int[0];
+            System.out.println(scan.nextInt());
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] != val) {
+                    nums[i] = nums[j];
+                    i++;
+                }
             }
         }
         return i;
     }
-
-    public static void main(String[] args) {
+    public static void main (String[]args){
         RemoveElement re = new RemoveElement();
         re.removeElement(2);
     }

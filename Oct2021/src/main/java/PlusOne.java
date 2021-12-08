034d3f500bc7751f5f93bcb29ca45f5c41b2d1e4
vi1801 <vidalwadi11@gmail.com>
2021-12-07 19:00:31 -0800

    adding plus one to array.

---
+++ Oct2021/src/main/java/PlusOne.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class PlusOne {
+    public static int[] plusOne(int digits[]) {
+        int i = digits.length-1;
+        while(i>=0){
+            if(digits[i]!=9){
+                digits[i] = digits[i]+1;
+                return digits;
+            }
+            digits[i] = 0;
+            i--;
+        }
+        int[] res = new int[digits.length+1];
+        res[0] = 1;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] digits= {1,2,3};
+        plusOne(digits);
+        System.out.println(Arrays.toString(digits));
+    }
+}
